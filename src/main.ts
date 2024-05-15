b6cc71ce8a5853c749dc95e7140f30402f384667
julasweta <julasweta@ukr.net>
2024-05-15 10:29:42 +0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,37 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app.module';
+import { CustomConfigService } from './config/config.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cors from 'cors';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const appConfig: CustomConfigService =
+    app.get<CustomConfigService>(CustomConfigService);
+
+  app.use(cors());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
+  //swagger
+  const config = new DocumentBuilder()
+    .setTitle('Programming School')
+    .setDescription('Description Project')
+    .setVersion('1.0.')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(appConfig.app_port, () => {
+    Logger.log(`http://${appConfig.app_host}:${appConfig.app_port}/api`, 'Doc');
+  });
+}
+bootstrap();
